ba64a4ef9476b527053c9104f99380ff9a7b4c77
mattBtindall <matthewbtindall@gmail.com>
2023-05-22 12:21:42 +0100

    Style and add sidebar for mobiles and desktop
    Mobiles: Opens sidebar on icon click
    Desktop: Displays by default to the left of the posts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,22 @@
-console.log('this is the hello world ')
+window.onload = function() {
+    const sidebarIconEl = document.querySelector('.sidebar-icon')
+    const sidebarEl = document.querySelector('.sidebar')
+    const bodyOverlayEl = document.querySelector('.body-overlay')
+
+    function toggleSidebar() {
+        sidebarEl.classList.toggle('active')
+        document.body.classList.toggle('active')
+    }
+
+    sidebarIconEl.addEventListener('click', function() {
+        // if position is fixed then we want to be able to open and close the side bar
+        const sideBarPosition = window.getComputedStyle(sidebarEl).getPropertyValue("position")
+        if (sideBarPosition !== 'fixed') return
+
+            toggleSidebar()
+        })
+
+    bodyOverlayEl.addEventListener('click', function() {
+        toggleSidebar()
+    })
+}
--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +11,17 @@ class PostController extends AbstractController
     #[Route('/posts', name: 'app')]
     public function index(): Response
     {
+        $user = [
+            'name' => 'geoff',
+            'email' => 'geoff@gmail.com',
+            'following' => ['test1', 'test2', 'test3'],
+            'followers' => ['test1', 'test2', 'test3', 'test4', 'test5'],
+            'avatar' => 'default.png'
+        ];
+
         return $this->render('post/posts.html.twig', [
-            'pageTitle' => 'Home'
+            'pageTitle' => 'Home',
+            'user' => $user
         ]);
     }
 }
