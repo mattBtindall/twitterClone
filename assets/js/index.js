868ac582698f917ba2f3c54fcdf5cd44b31f557d
mattBtindall <matthewbtindall@gmail.com>
2023-05-24 11:16:58 +0100

    Extract eventListeners into seperate js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,9 @@
 import { getAbsoluteHeight } from "./utils"
+import { setEventlisteners } from "./eventListeners"
 
 window.onload = function() {
-    const sidebarIconEl = document.querySelector('.sidebar-icon')
-    const sidebarEl = document.querySelector('.sidebar')
-    const bodyOverlayEl = document.querySelector('.body-overlay')
-
-    function toggleSidebar() {
-        sidebarEl.classList.toggle('active')
-        document.body.classList.toggle('active')
-    }
-
-    sidebarIconEl.addEventListener('click', function() {
-        // if position is fixed then we want to be able to open and close the side bar
-        const sideBarPosition = window.getComputedStyle(sidebarEl).getPropertyValue("position")
-        if (sideBarPosition !== 'fixed') return
-        toggleSidebar()
-    })
-
-    bodyOverlayEl.addEventListener('click', function() {
-        toggleSidebar()
-    })
-
     setMainContainerHeight()
+    setEventlisteners()
 }
 
 function setMainContainerHeight() {
