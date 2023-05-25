fd2450db6be90bd7533c91e52a29c2fcab29f01d
mattBtindall <matthewbtindall@gmail.com>
2023-05-25 11:00:35 +0100

    Add footer

--- assets/js/DomManipulation.js
+++ assets/js/DomManipulation.js
@@ -3,15 +3,24 @@ import { getAbsoluteHeight } from "./utils"
 export class DomManipulation {
     constructor(sharedElements) {
         this.elements = {
-            mainContentEl: document.querySelector('.main-content'),
+            mainContentEl: document.querySelector('.main-content-container'),
             mainHeaderEl: document.querySelector('.main-header')
 
         }
+
         this.setMainContainerHeight()
+        this.setYear()
     }
 
     setMainContainerHeight() {
         const mainContentHeight = window.innerHeight - getAbsoluteHeight(this.elements.mainHeaderEl)
         this.elements.mainContentEl.style.height = mainContentHeight + 'px'
+    }
+
+    setYear() {
+        const yearOutputs = document.querySelectorAll('.year-output')
+        const date = new Date()
+        const year = date.getFullYear()
+        yearOutputs.forEach(el => el.innerText = year)
     }
 }
