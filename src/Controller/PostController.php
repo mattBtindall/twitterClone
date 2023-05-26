8cbbf2a33851b21278b0d2fa50d013e09455aa79
mattbtindall <matthewbtindall@gmail.com>
2023-05-26 18:41:53 +0100

    Add posts templates

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,9 +20,31 @@ class PostController extends AbstractController
             'avatar' => 'default.png'
         ];
 
+        $posts = [
+            [
+                'id' => 1,
+                'user' => [
+                    'name' => 'Don Norman',
+                    'email' => 'donnorman@gmail.com',
+                ],
+                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt nihil rem, dolores commodi, fugit unde distinctio minima inventore id temporibus repellendus magni culpa debitis natus laboriosam reprehenderit. Veniam, facere?',
+                'created' => new DateTime()
+            ],
+            [
+                'id' => 2,
+                'user' => [
+                    'name' => 'Matt Tindall',
+                    'email' => 'mattbtindall@gmail.com',
+                ],
+                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore deserunt nihil rem, dolores commodi, fugit unde distinctio minima inventore id temporibus repellendus magni culpa debitis natus laboriosam reprehenderit. Veniam, facere?',
+                'created' => new DateTime()
+            ],
+        ];
+
         return $this->render('post/posts.html.twig', [
             'pageTitle' => 'Home',
-            'user' => $user
+            'user' => $user,
+            'posts' => $posts
         ]);
     }
 }
