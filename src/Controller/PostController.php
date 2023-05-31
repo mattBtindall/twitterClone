a1b3670df45c0818db4e13b4379c37a41c9437fb
mattBtindall <matthewbtindall@gmail.com>
2023-05-31 08:46:20 +0100

    Add post show route with template

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -11,13 +11,29 @@ require_once '../public/Constants/Constants.php';
 
 class PostController extends AbstractController
 {
-    #[Route('/posts', name: 'app')]
+    #[Route('/posts', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('post/posts.html.twig', [
             'pageTitle' => 'Home',
             'user' => USER,
             'posts' => POSTS
+        ]);
+    }
+
+    #[Route('/post/{id}', name: 'app_show')]
+    public function show($id): Response
+    {
+        $post = array_values(array_filter(POSTS, function($value) use($id) {
+            if ($value['id'] == $id) {
+                return true;
+            }
+        }))[0];
+
+        return $this->render('post/post.html.twig', [
+            'pageTitle' => 'Show',
+            'user' => USER,
+            'post' => $post
         ]);
     }
 }
