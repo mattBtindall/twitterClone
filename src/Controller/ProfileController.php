f261b4628dd55fa1ee8519f22e00096cffd168b5
mattbtindall <matthewbtindall@gmail.com>
2023-06-02 11:11:02 +0100

    Add profile links in
    For sidebar and posts headers

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -11,12 +11,23 @@ require_once '../public/Constants/Constants.php';
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    #[Route('/profile/{id?}', name: 'app_profile', defaults: ['id' => null])]
+    public function index($id): Response
     {
+        if ($id === null) {
+            $user = USER;
+        } else {
+            $filteredUser = array_values(array_filter(USERS, function($user) use($id) {
+                if ($user['id'] == $id) {
+                    return true;
+                }
+            }));
+            $user = $filteredUser[0];
+        }
+
         return $this->render('profile/profile.html.twig', [
             'pageTitle' => 'Profile',
-            'user' => USER
+            'user' => $user
         ]);
     }
 }
