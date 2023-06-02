ff26d510c2af295d2a4e8b7d2eb4c75f325b77fa
mattbtindall <matthewbtindall@gmail.com>
2023-06-02 11:10:53 +0100

    Update test data

--- public/Constants/Constants.php
+++ public/Constants/Constants.php
@@ -1,16 +1,9 @@
 <?php
-const USER = [
-    'name' => 'geoff',
-    'email' => 'geoff@gmail.com',
-    'following' => ['test1', 'test2', 'test3'],
-    'followers' => ['test1', 'test2', 'test3', 'test4', 'test5'],
-    'avatar' => 'default.png'
-];
-
 const POSTS = [
     [
-        'id' => 1,
+        'id' => 0,
         'user' => [
+            'id' => 0,
             'name' => 'Don Norman',
             'email' => 'donnorman@gmail.com',
         ],
@@ -31,8 +24,9 @@ const POSTS = [
         ]
     ],
     [
-        'id' => 2,
+        'id' => 1,
         'user' => [
+            'id' => 1,
             'name' => 'Paul Dingle',
             'email' => 'pauldingleberries@gmail.com',
         ],
@@ -47,8 +41,9 @@ const POSTS = [
         ]
     ],
     [
-        'id' => 3,
+        'id' => 2,
         'user' => [
+            'id' => 2,
             'name' => 'Matt Tindall',
             'email' => 'mattbtindall@gmail.com',
         ],
@@ -57,4 +52,83 @@ const POSTS = [
         'likedBy' => [],
         'comments' => []
     ],
+    [
+        'id' => 0,
+        'user' => [
+            'id' => 0,
+            'name' => 'Don Norman',
+            'email' => 'donnorman@gmail.com',
+        ],
+        'content' => 'Some test post, lorem boop blap bleep',
+        'created' => new DateTime(),
+        'likedBy' => [
+            ['name' => 'Paul Dingle', 'email' => 'pauldingleberries@gmail.com']
+        ],
+        'comments' => [
+            [
+                'content' => 'This is just a random comment',
+                'user' => ['name' => 'Paul Dingle','email' => 'pauldingleberries@gmail.com']
+            ],
+            [
+                'content' => 'This is just another random comment',
+                'user' => ['name' => 'Matt Tindall','email' => 'mattbtindall@gmail.com']
+            ],
+        ]
+    ],
 ];
+
+const USERS = [
+    [
+        'id' => 0,
+        'name' => 'Don Norman',
+        'email' => 'donnorman@gmail.com',
+        'following' => ['test1', 'test2', 'test3'],
+        'followers' => ['test1', 'test2', 'test3', 'test4', 'test5'],
+        'avatar' => 'default.png',
+        'posts' => [POSTS[0]],
+        'likes' => [],
+        'bio' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos asperiores a consectetur repudiandae vero quidem qui aperiam iusto.',
+        'location' => [
+            'townCity' => 'London',
+            'country' => 'United Kingdom'
+        ],
+        'createdAt' => new DateTime(),
+        'websiteUrl' => 'www.google.com'
+    ],
+    [
+        'id' => 1,
+        'name' => 'Paul Dingle',
+        'email' => 'pauldingleberries@gmail.com',
+        'following' => ['test1'],
+        'followers' => ['test1', 'test2'],
+        'avatar' => 'default.png',
+        'posts' => [POSTS[1]],
+        'likes' => [POSTS[0]],
+        'bio' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis placeat quisquam impedit exercitationem eligendi ipsum ipsam molestiae quos molestias.',
+        'location' => [
+            'townCity' => 'Huddersfield',
+            'country' => 'United Kingdom'
+        ],
+        'createdAt' => new DateTime(),
+        'websiteUrl' => 'getbootstrap.com.com'
+    ],
+    [
+        'id' => 2,
+        'name' => 'Matt Tindall',
+        'email' => 'mattbtindall@gmail.com',
+        'following' => ['test1'],
+        'followers' => ['test1', 'test2'],
+        'avatar' => 'default.png',
+        'posts' => [POSTS[2]],
+        'likes' => [],
+        'bio' => 'Lorem ipsum dolor sit, amet consectetur adipisicing.',
+        'location' => [
+            'townCity' => 'Florida',
+            'country' => 'USA'
+        ],
+        'createdAt' => new DateTime(),
+        'websiteUrl' => 'facebook.com'
+    ]
+];
+
+const USER = USERS[1];
