c6bcbb8cce61cb62b024a66c904844a673b48e2c
mattBtindall <matthewbtindall@gmail.com>
2023-06-07 09:10:39 +0100

    Add fixtures with Post fixtures
    Add fixtures bundle via cli as dev dependency
    Create Post fixtures which adds a single post to the db

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
