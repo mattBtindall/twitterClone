cf585c63b28d0fdefc3d187ce825e184ab91ea73
mattBtindall <matthewbtindall@gmail.com>
2023-06-12 14:59:03 +0100

    Add userProfile form to user registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use App\Form\UserProfileType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -54,6 +55,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
+            ->add('profile', UserProfileType::class)
         ;
     }
 
