6c654e5d60d7612661cd81ad6234e3d04d4001a3
mattbtindall <matthewbtindall@gmail.com>
2023-06-15 07:58:02 +0100

    Update user profile pages
    Disable likes, following and followers pages until this functionality is added

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,97 +2,53 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // Used for test data
-require_once '../public/Constants/Constants.php';
+// require_once '../public/Constants/Constants.php';
 
 class ProfileController extends AbstractController
 {
-    #[Route('/profile/{id?}', name: 'app_profile_posts', defaults: ['id' => null])]
-    public function index($id): Response
+    #[Route('/profile/{id}', name: 'app_profile_posts')]
+    public function index(User $user): Response
     {
-        if ($id === null) {
-            $user = USER;
-        } else {
-            $filteredUser = array_values(array_filter(USERS, function($user) use($id) {
-                if ($user['id'] == $id) {
-                    return true;
-                }
-            }));
-            $user = $filteredUser[0];
-        }
-
         return $this->render('profile/profile.html.twig', [
-            'pageTitle' => $user['name'],
+            'pageTitle' => $user->getProfile()->getName(),
             'user' => $user,
             'activeTab' => 'posts'
         ]);
     }
 
-    #[Route('/profile/{id?}/likes', name: 'app_profile_likes', defaults: ['id' => null])]
-    public function likes($id): Response
+    #[Route('/profile/{id}/likes', name: 'app_profile_likes')]
+    public function likes(User $user): Response
     {
-        if ($id === null) {
-            $user = USER;
-        } else {
-            $filteredUser = array_values(array_filter(USERS, function($user) use($id) {
-                if ($user['id'] == $id) {
-                    return true;
-                }
-            }));
-            $user = $filteredUser[0];
-        }
-
         return $this->render('profile/likes.html.twig', [
-            'pageTitle' => $user['name'],
+            'pageTitle' => $user->getProfile()->getName(),
             'user' => $user,
             'activeTab' => 'likes'
         ]);
     }
 
-    #[Route('/profile/{id?}/following', name: 'app_profile_following', defaults: ['id' => null])]
-    public function following($id): Response
+    #[Route('/profile/{id}/following', name: 'app_profile_following')]
+    public function following(User $user): Response
     {
-        if ($id === null) {
-            $user = USER;
-        } else {
-            $filteredUser = array_values(array_filter(USERS, function($user) use($id) {
-                if ($user['id'] == $id) {
-                    return true;
-                }
-            }));
-            $user = $filteredUser[0];
-        }
-
         return $this->render('profile/following.html.twig', [
-            'pageTitle' => $user['name'],
+            'pageTitle' => $user->getProfile()->getName(),
             'user' => $user,
             'activeTab' => 'following'
         ]);
     }
 
-    #[Route('/profile/{id?}/followers', name: 'app_profile_followers', defaults: ['id' => null])]
-    public function followers($id): Response
+    #[Route('/profile/{id}/followers', name: 'app_profile_followers')]
+    public function followers(User $user): Response
     {
-        if ($id === null) {
-            $user = USER;
-        } else {
-            $filteredUser = array_values(array_filter(USERS, function($user) use($id) {
-                if ($user['id'] == $id) {
-                    return true;
-                }
-            }));
-            $user = $filteredUser[0];
-        }
-
-        return $this->render('profile/followers.html.twig', [
-            'pageTitle' => $user['name'],
+        return $this->render('profile/following.html.twig', [
+            'pageTitle' => $user->getProfile()->getName(),
             'user' => $user,
-            'activeTab' => 'followers'
+            'activeTab' => 'following'
         ]);
     }
-
 }
