9070765cdc757d10ae9c748323bc37be32a34ad6
mattbtindall <matthewbtindall@gmail.com>
2023-06-18 14:35:33 +0100

    Add main post input as a symfony Form
    Previously this was just added as standard html inputs but this is bad practice as if data was submitted then the validation and security checks would not take place

---
+++ src/Form/MainInputType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MainInputType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('post', TextareaType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
